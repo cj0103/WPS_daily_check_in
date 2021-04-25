9615873671cedf5db20ca87c7b9c3cf4138fcd8f
cj0103 <66166450+cj0103@users.noreply.github.com>
2021-04-25 12:15:05 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -22,7 +22,7 @@ mk = 0
 
 def request_re(sid, invite_userid, rep = 30):
     invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
-    r = requests.post(invite_url, headers={'sid': sid}, data={'invite_userid': invite_userid})
+    r = requests.post( invite_url, headers={ 'sid': sid }, data={ 'invite_userid': invite_userid, "client_code": "040ce6c23213494c8de9653e0074YX30", "client": "alipay" } )
     js = json.loads(r.content)
     if js['msg'] == 'tryLater' and rep > 0:
         rep -= 1
@@ -36,7 +36,8 @@ for i in invite_userids:
         js = json.loads(r.content)
         if js['result'] == 'ok':
             mk += 1
-            
+
+time.sleep(10)            
 print('成功邀请%d位好友'%(mk))   
 
 SERVER_KEY = os.getenv('SERVER_KEY')
